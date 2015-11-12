f35e4133a6b300ac756537616d80106849257b6d
Will Rice <willrice21@gmail.com>
2015-11-12 16:35:54 -0500

    tested locality

--- testshit.java
+++ testshit.java
@@ -4,11 +4,26 @@ import java.util.regex.Pattern;
 public class testshit {
 	
 	public static void main(String args[]){
-		Pattern pattern = Pattern.compile("(\\w+(\\d*(:*))*)");
-		Matcher matcher;
-		matcher = pattern.matcher("add byung  11:30:14:02:28:08  5  5000000  0");
-		while(matcher.find()){
-			System.out.println(matcher.group());
+			int array[] = {0,1,2};
+			
+			for(int i : array){
+				System.out.println(i);
+			}
+			
+			change(array);
+			
+			for(int i : array){
+				System.out.println(i);
+			}
+				
+	}
+	
+	public static void change(int p[]){
+		
+		for(int i : p){
+			p = p.clone();
+			p[i]++;
+			System.out.println(p[i]);
 		}
 	}
 
