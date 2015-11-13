1c86f04d093b8a13263a05ab503751ae0b7c94fd
Will Rice <willrice21@gmail.com>
2015-11-12 22:58:43 -0500

    ????????

--- testshit.java
+++ testshit.java
@@ -1,21 +1,21 @@
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class testshit {
 	
+	static ArrayList<Integer> LSD[] = (ArrayList<Integer>[]) new ArrayList[10];
+	
+	
 	public static void main(String args[]){
 			
-		int i;
+		System.out.println(getDigit(2,2));
 				
 	}
 	
-	public static void change(int p[]){
-		
-		for(int i : p){
-			p = p.clone();
-			p[i]++;
-			System.out.println(p[i]);
-		}
+	public static int getDigit(int num, int digit){
+		return (int) ((num/Math.pow(10, digit-1))%10);
 	}
+	
 
 }
