54a80de01aae849877b93e92618c5a52bed90e6a
Will Rice <willrice21@gmail.com>
2015-11-12 22:58:31 -0500

    implemented LSD radix sort

--- Sorting.java
+++ Sorting.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sorting {
@@ -14,6 +15,7 @@ public class Sorting {
 	static int[] r_10k = new int[10000];
 	static int[] r_100k = new int[100000];
 	static int[] r_1m = new int[1000000];
+	static ArrayList<Integer>[] LSD = new ArrayList[10];
 
 	public static void main(String args[]) throws FileNotFoundException {
 
@@ -24,8 +26,9 @@ public class Sorting {
 		r_100k = readFile("r_100k.out",r_100k);
 		r_1m = readFile("r_1m.out",r_1m);
 		
+		System.out.println(LSDRadixTimer(r_1m));
 		System.out.println(quickSortTimer(r_1m));
-		System.out.println(insertionSortTimer(r_100, 0 , r_100.length));
+		System.out.println(insertionSortTimer(r_1m, 0 , r_1m.length));
 		/*System.out.println(quickSortTimer(r_100));
 		System.out.println(quickSortTimer(r_1k));
 		System.out.println(quickSortTimer(r_10k));
@@ -61,7 +64,7 @@ public class Sorting {
 		}
 	}
 
-	public Long shellSort(int[] array) {
+	public static Long shellSort(int[] array) {
 		array = array.clone();
 		start = System.nanoTime();
 		end = System.nanoTime();
@@ -73,10 +76,6 @@ public class Sorting {
 		start = System.nanoTime();
 		quickSort(array,0,array.length-1);
 		end = System.nanoTime();
-		for (int y : array) {
-			System.out.print(y + ", ");
-		}
-		System.out.println("");
 		return (end - start);
 	}
 	
@@ -119,12 +118,36 @@ public class Sorting {
 		}
 	}
 
-	public Long LSDRadix(int[] array) {
+	public static Long LSDRadixTimer(int[] array) {
 		array = array.clone();
+		for(int i = 0; i < 10; i++)
+			LSD[i] = new ArrayList<Integer>();
 		start = System.nanoTime();
+		LSDRadix(array);
 		end = System.nanoTime();
 		return (end - start);
 
+	}
+	
+	public static void LSDRadix(int[] array){
+		for(int d = 1; d < 8 ; d++){
+			for(int i : array){
+				LSD[getDigit(i,d)].add(i);
+			}
+			for(int i = 1; i < 10; i++){
+				LSD[0].addAll(LSD[i]);
+				LSD[i].clear();
+			}
+			int j = 0;
+			for(int i : LSD[0]){
+				array[j++] = i;
+			}
+			LSD[0].clear();
+		}
+	}
+	
+	public static int getDigit(int num, int digit){
+		return (int) ((num/Math.pow(10, digit-1))%10);
 	}
 	
 	public static int[] readFile(String name, int[] array) throws FileNotFoundException{
