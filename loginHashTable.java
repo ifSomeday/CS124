f25ecc2b448d54959cf90644f11fff6ff9b28193
Will Rice <willrice21@gmail.com>
2015-12-05 16:22:05 -0500

    create hashtable class

---
+++ loginHashTable.java
@@ -0,0 +1,47 @@
+/*
+ * Will Rice
+ * CS 124
+ * all code written by Will Rice.
+ * Copying code without permission is expressly prohibited.
+ * quadratic probing, double and rehash if 50% full, halve and rehash if 15% full
+ */
+public class loginHashTable {
+	
+	
+	loginHashTable(){
+		
+	}
+	
+	public void register(String user, String pass){
+		
+	}
+	
+	public void unregister(String user){
+		
+	}
+	
+	public void list(){
+		
+	}
+	
+	public void login(String user, String pass){
+		
+	}
+	
+	public void logout(String user){
+		
+	}
+	
+	public void users(){
+		
+	}
+	
+	public void inspect(int property){
+		
+	}
+	
+	public void dump(){
+		
+	}
+}
+
--- loginManager.java
+++ loginManager.java
@@ -1,4 +1,14 @@
-
+/*
+ * Will Rice
+ * CS 124
+ * all code written by Will Rice.
+ * Copying code without permission is expressly prohibited.
+ */
 public final class loginManager {
-
+	
+	static boolean state;
+	
+	public static void main(String[] args){
+		
+	}
 }
